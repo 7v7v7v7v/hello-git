f401c3435352bb3d7cf403a13603c7571433a953
unknown <729933830@qq.com>
2022-06-02 09:43:41 +0800

    无敌帅到爆

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<el-time-picker v-model="value1" placeholder="Arbitrary time" />
+</template>
+
+<script>
+import { Calendar, Search } from '@element-plus/icons-vue'
+export default {
+  data() {
+    return {
+      text:null,
+      Calendar,
+    }
+  }
+} 
+</script>
